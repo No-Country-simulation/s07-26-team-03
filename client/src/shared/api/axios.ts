85954c0c1b387eae01a75c65a49f0150be509c67
schweigenderFlugel <facundoezcastro@gmail.com>
2026-07-28 19:01:22 -0300

    chore: changes on axios config

--- client/src/shared/api/axios.ts
+++ client/src/shared/api/axios.ts
@@ -1,23 +1,22 @@
 import axios from "axios";
 
 import { env } from "@/app/config/env";
-import { setupInterceptors } from "./interceptors";
-
-export const api = axios.create({
-  baseURL: env.API_URL,
-  timeout: 30000,
-
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
-
-
-setupInterceptors(api);
 
 export const publicApi = axios.create({
 
     baseURL: env.API_URL
 
+});
+
+export const api = axios.create({
+  baseURL: env.API_URL,
+  timeout: 30000,
+  withCredentials: true,
+
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-origin": "*",
+    'Access-Control-Allow-Credentials': 'true',
+  },
 });
