7baf815bd4f5f4dfe73f1c7a31a1ab6d964995ab
schweigenderFlugel <facundoezcastro@gmail.com>
2026-07-28 22:47:59 -0300

    chore: implement useRefresh hook on context

--- client/src/shared/context/AuthContext.tsx
+++ client/src/shared/context/AuthContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useState, type Dispatch, type SetStateAction, type ReactNode } from "react";
+import { createContext, useState, type Dispatch, type SetStateAction, type ReactNode, useRef, useEffect } from "react";
+import useRefresh from "../hooks/useRefresh";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export interface IAuthContext {
   accessToken: string | null;
@@ -19,26 +21,27 @@ const AuthContext = createContext<IAuthContext>(defaultState);
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [accessToken, setAccessToken] = useState<string | null>(null)
 
-  // const navigate = useNavigate();
-  // const location = useLocation();
-  // const hasFetched = useRef(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const hasFetched = useRef(false);
 
-  // const from = location.state?.from?.pathname;
+  const from = location.state?.from?.pathname;
 
-  // const { setRefresh } = useRefresh({
-  //   onSuccess: () => {
-  //     navigate(from, { replace: true });
-  //   },
-  //   onReject: () => {},
-  //   setToken: (token: string) => setAccessToken(token),
-  // })
+  const { setRefresh } = useRefresh({
+    onSuccess: () => {
+      navigate(from, { replace: true });
+    },
+    onReject: () => {},
+    setToken: (token: string) => setAccessToken(token),
+  })
 
-  // useEffect(() => {
-  //   if (!hasFetched.current) {
-  //     hasFetched.current = true;
-  //     setRefresh();
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (!hasFetched.current) {
+      hasFetched.current = true;
+      setRefresh();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <AuthContext.Provider 
