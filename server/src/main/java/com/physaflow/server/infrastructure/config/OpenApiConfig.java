35565fea4c988786ec88f9cdde8eaf5408c1c57c
Diegith <diego.albarracinacero@gmail.com>
2026-08-03 18:10:30 -0500

    feat(infra): configure security rules and OpenAPI contracts

--- server/src/main/java/com/physaflow/server/infrastructure/config/OpenApiConfig.java
+++ server/src/main/java/com/physaflow/server/infrastructure/config/OpenApiConfig.java
@@ -1,4 +1,21 @@
 package com.physaflow.server.infrastructure.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI physaFlowOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PhysaFlow Core Engine API")
+                        .description("Documentación oficial del motor analítico y transaccional de PhysaFlow para el cálculo de capacidad varada (Stranded MW) y desperdicio energético en centros de datos.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
 }
