c196bd1bc8dff820989823e49a5cf2b48ce96c35
schweigenderFlugel <facundoezcastro@gmail.com>
2026-08-03 00:48:22 -0300

    feat: add github workflow to push docker image to repository

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "🔧 Injecting environment variables..."
 
-envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
+envsubst < /usr/share/nginx/html/env.js > /usr/share/nginx/html/env.js
 
 echo "===== env.js ====="
 cat /usr/share/nginx/html/env.js
