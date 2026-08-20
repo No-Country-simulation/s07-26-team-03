cfb4becd6ff3e2241572ff310a67a9ead32d9361
schweigenderFlugel <facundoezcastro@gmail.com>
2026-08-20 17:58:29 -0300

    chore: fix errors

--- client/src/shared/api/axios.ts
+++ client/src/shared/api/axios.ts
@@ -4,7 +4,7 @@ import { env } from "@/app/config/env";
 
 export const publicRoutes = axios.create({
     baseURL: env.API_URL,
-    timeout: 10000,
+    timeout: 30000,
     withCredentials: true,
     headers: {
       Accept: "application/json",
